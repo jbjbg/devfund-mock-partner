ac5273cf843d06c74327f946c7206550e9b33c2f
jacob <1anderson2jacob@gmail.com>
2019-02-26 12:38:41 -0800

    fake company's key server

---
+++ company-server/server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const uuid = require('uuid');
+
+
+app.use(express.json());
+
+app.get('/', (err, req, res) => {
+  if(err) {
+    // res.error(503);
+    throw err;
+  }
+  let id = uuid();
+  res.json(id);
+});
+
+
+module.exports = {
+  server: app,
+  start: () => {
+    const PORT = process.env.port || 3030;
+    app.listen(PORT, () => console.log(`Server up on port ${PORT}`));
+  },
+};
